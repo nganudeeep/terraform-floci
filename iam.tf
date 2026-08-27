0d07b0134936959b8153e37fb7a09ce1b5c3bba9
nganudeeep <153569333+nganudeeep@users.noreply.github.com>
2026-08-27 12:02:01 -0700

    created policy role and role association with ec2 instance profile

---
+++ iam.tf
@@ -0,0 +1,81 @@
+#iam policy to read s3
+resource "aws_iam_policy" "s3_read" {
+  name        = "s3-read-policy"
+  description = "Allow read access to S3"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+#one more policy to read secrets
+resource "aws_iam_policy" "secrets_read" {
+  name        = "secrets-read-policy"
+  description = "Allow read access to Secrets Manager"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Resource = aws_secretsmanager_secret.app_secret.arn
+      }
+    ]
+  })
+}
+
+
+#iam role creation
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-devops-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach S3 read policy to EC2 role
+resource "aws_iam_role_policy_attachment" "s3_read" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_read.arn
+}
+
+# Attach Secrets Manager read policy to EC2 role
+resource "aws_iam_role_policy_attachment" "secrets_read" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.secrets_read.arn
+}
+
+#Create an EC2 instance profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-devops-profile"
+  role = aws_iam_role.ec2_role.name
+}
--- provider.tf
+++ provider.tf
@@ -17,7 +17,9 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    ec2 = "http://localhost:4566"
-    s3  = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    iam            = "http://localhost:4566"
   }
 }
